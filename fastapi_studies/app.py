0c6e0812195292c5ed02b3c33ef662cd5548a9e6
Lucas Schilin <schilin.lucas@gmail.com>
2025-04-07 06:25:43 -0300

    chore: update api version from 0.0.1 to 0.1.0

--- fastapi_studies/app.py
+++ fastapi_studies/app.py
@@ -6,7 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi_studies.routers import auth, me, users
 from fastapi_studies.schemas.message import MessageSchema
 
-app = FastAPI(version='0.0.1')
+app = FastAPI(version='0.1.0')
 
 app.add_middleware(
     CORSMiddleware,
