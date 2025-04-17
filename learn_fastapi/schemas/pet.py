1fdcb127343d2df058426cc7b647f8b3b20839f5
Lucas Schlindwein <lucas.schlindwein@aprimorar.net.br>
2025-04-17 11:25:05 -0300

    refactor: change eSpecies to eSpecie in pet Schema and 'task format' runned

--- learn_fastapi/schemas/pet.py
+++ learn_fastapi/schemas/pet.py
@@ -1,30 +1,36 @@
 from enum import IntEnum
+
 from pydantic import BaseModel
 
 from learn_fastapi.schemas.user import GetUserSchema
 
-class eSpecies(IntEnum):
-    CAT = 1
-    DOG = 2
+
+class eSpecie(IntEnum):
+    DOG = 1
+    CAT = 2
     HAMSTER = 3
     SNAKE = 4
 
+
 class CreatePetSchema(BaseModel):
     name: str
-    specie: eSpecies
+    specie: eSpecie
     mother: int
     father: int
     tutor: int
+
 
 class GetPetSchema(BaseModel):
     id: int
     mother: int
     father: int
     name: str
-    specie: eSpecies
+    specie: eSpecie
+
 
 class GetPetWithOwnersSchema(GetPetSchema):
     owners: list[GetUserSchema]
+
 
 class GetPetsSchema(BaseModel):
     pets: list[GetPetSchema]
